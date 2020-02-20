f6b8afe3ac108e16fc53ac889979e508c1e477ef
Alex-GD12 <59962597+Alex-GD12@users.noreply.github.com>
2020-02-20 17:19:30 +0100

    Update CRACCO_BEAUCHAMP_game.c

--- CRACCO_BEAUCHAMP_game.c
+++ CRACCO_BEAUCHAMP_game.c
@@ -32,62 +32,62 @@ int main () {
 	
 	scanf("\n%d", &repPlayer);
 	
-	while(repPlayer!= 1 && repPlayer!= 2 && repPlayer!= 3){
+	if(repPlayer!= 1 && repPlayer!= 2 && repPlayer!= 3){
 		printf("'Repondez a la question correctement nom d'un bling !'\n");
 		scanf("%d", &repPlayer);
 		
 	}
 	
 	
-		//SCENAR ROXANE
-		if (repPlayer== 1){
-			tamponRepPlayer ==1;
-			printf("\nVous arrivez a l'infirmerie et vous tombez nez a nez avec Fransiscain le Praticien.\n");
-			printf("\nFransiscain : Mon enfant, etes vous l'eleve qui viens effectuer son stage chez mon eminente et neanmoins illuminee collegue Roxane la Shamane ?\n"); 
-			printf("\nRoxane n'est pas ici tres cher, elle est dans les jardins de l'ecole.\n");
-			printf ("\n1) Se rendre dans les jardins pour trouver Roxane. \n");
-			printf ("\n2) Demander a Fransiscain si il peut s'occupper de vous a la place de Roxane. \n");
-			printf ("\n3) Retourner voir Tapadkeur pour lui dire que Roxane n'est pas la. \n");
-			
-			scanf("%d", &repPlayer);
-				if (repPlayer== 1 && tamponRepPlayer == 1){
-					
-					
-				}
+	//SCENAR ROXANE
+	if (repPlayer== 1){
+		tamponRepPlayer ==1;
+		printf("\nVous arrivez a l'infirmerie et vous tombez nez a nez avec Fransiscain le Praticien.\n");
+		printf("\nFransiscain : Mon enfant, etes vous l'eleve qui viens effectuer son stage chez mon eminente et neanmoins illuminee collegue Roxane la Shamane ?\n"); 
+		printf("\nRoxane n'est pas ici tres cher, elle est dans les jardins de l'ecole.\n");
+		printf ("\n1) Se rendre dans les jardins pour trouver Roxane. \n");
+		printf ("\n2) Demander a Fransiscain si il peut s'occupper de vous a la place de Roxane. \n");
+		printf ("\n3) Retourner voir Tapadkeur pour lui dire que Roxane n'est pas la. \n");
+		
+		scanf("%d", &repPlayer);
+			if (repPlayer== 1){
 				
-				if (repPlayer== 2 && tamponRepPlayer == 1){
-					
-					
-				}
 				
-				if (repPlayer == 3 && tamponRepPlayer == 1){
-					printf ("\nTapadkeur le Directeur : VOUS VOUS FOUTEZ DE MOOOOI ?! VOUS ETES VIRE ! VOUS ETES TELLEMENT VIRE QUE MEME SI JE VOUS REPRENAIS VOUS SERIEZ VIRE PAR DEFAUT ! \n");
-					printf ("\n Vous avez mis le directeur en colère, vous avez perdu, il vaudrait mieux reessayer.... \n");
-					
-				}
+			}
 			
+			if (repPlayer== 2){
+				
+				
+			}
 			
-		}
-		
-		//SCENAR POULET
-		if (repPlayer== 2){
-			tamponRepPlayer ==2;
-			printf("\nVous arrivez dans les cuisines. \n");
-			
-			
-			
-			
-		}
+			if (repPlayer == 3){
+				printf ("\nTapadkeur le Directeur : VOUS VOUS FOUTEZ DE MOOOOI ?! VOUS ETES VIRE ! VOUS ETES TELLEMENT VIRE QUE MEME SI JE VOUS REPRENAIS VOUS SERIEZ VIRE PAR DEFAUT ! \n");
+				printf ("\n Vous avez mis le directeur en colère, vous avez perdu, il vaudrait mieux reessayer.... \n");
+				
+			}
 		
 		
-		//SCENAR GRENDEL
-		if (repPlayer== 3){
-			tamponRepPlayer ==3;
-			printf("\n Vous arrivez devant la vie scolaire.\n");
-			
-			
-			
-			
+	}
+	
+	//SCENAR POULET
+	if (repPlayer== 2){
+		tamponRepPlayer ==2;
+		printf("\nVous arrivez dans les cuisines. \n");
+		
+		
+		
+		
+	}
+	
+	
+	//SCENAR GRENDEL
+	if (repPlayer== 3 ){
+		tamponRepPlayer ==3;
+		printf("\n Vous arrivez devant la vie scolaire.\n");
+		
+		
+		
+		
 		}
 	
 	
