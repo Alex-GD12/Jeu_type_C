6c669b99737e114efb8ca4b6a32a9c4951a42bdf
Alex-GD12 <59962597+Alex-GD12@users.noreply.github.com>
2020-02-20 15:54:50 +0100

    Update CRACCO_BEAUCHAMP_game.c

--- CRACCO_BEAUCHAMP_game.c
+++ CRACCO_BEAUCHAMP_game.c
@@ -8,10 +8,17 @@
 
 //Fonctions 
 
-//Début
+
 int main () {
+	//variables
+	int repPlayer = 0;
+	int tamponRepPlayer = 0;
 	
-	
+	//Début
+	printf ("Bienvenu a l'ecole de magie Blingue Premier ! Vous etes un eleve qui a rate sa cinquieme annee et qui est convoque dans le bureau de Tapadkeur le Directeur...\n");
+	printf ("\n****************************************************************************************************************************************************************\n");
+	printf ("'Bon. Tres bien. *soupir* J'ai ici votre dossier, et croyez moi, son epaisseur n'est pas la pour vous rassurer.... Vos resultats sont a l'image de ma patience, ils s'affaiblissent chaque jour un peu plus.");
+	printf ("\nEtant donne votre niveau de base, vous pouvez comprendre que vos notes sont maintenant plus basses que les enfers. Et si vous ne voulez pas les y rejoindre, il faudra commencer a vous bougez un peu.'");
 	
 	return 0;
 }
